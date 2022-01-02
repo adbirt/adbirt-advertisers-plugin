dcbb12d8ed6ca78d9bd1f209447939d741ff5db1
danroyal001 <danroyaleffiong@gmail.com>
2022-01-02 22:07:42 +0100

    added autopilot to advertisers js

--- assets/js/adbirt.js
+++ assets/js/adbirt.js
@@ -1,150 +1,169 @@
+/**
+ * Advertiser script for AdbirtAdvertiser.com
+ */
 (() => {
-    class Adbirt {
-
+    class AdbirtAdvertiser {
+        /**
+         * Endpoint to be called to charge an advertiser when a campaign is consumed
+         */
         url = "https://adbirt.com/campaigns/verified";
+        /**
+         * Current campaign code to take action on
+         */
         camp_code = '';
+        /**
+         * Whether or not the current webpage is a valid campaign page
+         */
         isCampaignPage = false;
+        /**
+         * Request payload
+         */
         params = new URLSearchParams;
-
+        /**
+         * Key for holding data in browser storage
+         */
+        #storageKey = `adbirt_camp_code_${(new Date).getTime()}`;
+        /**
+         * URL object for the current page url
+         */
+        currentPageUrl = new URL(window.location.href);
 
         // --
-
 
         constructor() {
+            console.log('Constructed');
+            this.params = this.currentPageUrl.searchParams;
 
-            const currentPageUrl = new URL(window.location.href);
-            const params = this.params = currentPageUrl.searchParams;
-
-            this.camp_code = params.get('camp_code');
+            this.camp_code = this.params.get('camp_code');
             this.isCampaignPage = (this.camp_code) ? true : false;
 
+            this.isCampaignPage && this.autoPilot();
+
             return this;
-
         }
 
         // --
 
+        async autoPilot() {
+            console.log('adbirt auto pilot');
+            for (const mode of ['landing', 'success']) {
+                console.log(`For ${mode}`);
+                const newUrl = this.currentPageUrl;
+                newUrl.searchParams.delete('camp_code');
+                const req_url_encoded = encodeURIComponent(newUrl.toString());
 
-        redirectFormSubmitInit() {
+                const res = await fetch(`https://adbirt.com/api/check-if-url-is-valid-campaign?url_in_question=${req_url_encoded}&url_type=${mode}&code=${this.camp_code}`);
+                console.log(`Response for ${mode} is: `, res);
+                const json = await res.json();
+                console.log(`json for ${mode}: `, json);
 
-            try {
-                if (this.isCampaignPage) {
+                const is_valid = json['is_valid'] == 'true' ? true : false;
 
-                    let token = this.camp_code
-                    window.localStorage.setItem('redirect_camp_code', token);
-
-                    return token;
+                if (is_valid) {
+                    console.log(`${mode} is valid`);
+                    this.actionMap(mode);
+                } else {
+                    // not a campaign page
                 }
-                // else {
-                //     return window.localStorage.removeItem('redirect_camp_code');
-                // }
-            } catch (error) {
-                return error;
             }
-
         }
 
+        async actionMap(mode) {
+            console.log(`adbirt action map ${mode}`);
+            const map = {
+                'landing': async () => await this.redirectFormSubmitInit(),
+                'success': async () => await this.redirectFormSubmit()
+            }
+
+            return await map[mode]();
+        }
+
+        // --
+
+        /**
+         * @note Primary function 1
+         */
+        async redirectFormSubmitInit() {
+            if (this.isCampaignPage) {
+                try {
+                    window.sessionStorage.setItem(this.#storageKey, this.camp_code);
+                    console.log('adbirt campaign init');
+                } catch (error) {
+                    console.error(error);
+                }
+            } else {
+                this.noCampaignSpecified();
+            }
+        }
+
+        /**
+         * @note Primary function 2 
+         */
         async redirectFormSubmit() {
+            if (this.isCampaignPage) {
+                try {
+                    const token = window.sessionStorage.getItem(this.#storageKey);
 
-            try {
+                    if (token) {
+                        //make request to backend with token to check valid or not 
+                        const formUrlParams = new URLSearchParams();
+                        formUrlParams.append('campaign_code', token);
+                        try {
+                            const res = await this.makeApiCall(formUrlParams, 'Campaign Consumed');
+                            console.log(await res.text());
+                            window.sessionStorage.removeItem(this.#storageKey);
+                            console.log('adbirt campaign submit');
 
-                let token = window.localStorage.getItem('redirect_camp_code');
-
-                if (token) {
-                    //make request to backend with token to check valid or not 
-                    const formUrlParams = new URLSearchParams();
-                    formUrlParams.append('campaign_code', token);
-                    try {
-                        const res = await this.makeApiCall(formUrlParams, 'Redirect Form campaign success !!');
-                        console.log(await res.text());
-                        window.localStorage.removeItem('redirect_camp_code');
-
-                        return res;
-                    } catch (err) {
-                        console.log(err);
-                        // window.localStorage.removeItem('camp_code');
-
-                        return err;
+                            return res;
+                        } catch (err) {
+                            console.log(err);
+                            // window.sessionStorage.removeItem('camp_code');
+                            // if request failed, dont charge teh advertiser, just return
+                            return err;
+                        }
                     }
+                } catch (error) {
+                    return error;
                 }
-            } catch (error) {
-                return error;
+            } else {
+                this.noCampaignSpecified();
             }
-
         }
 
         // --
-
-
-        paymentPageInit() {
-            return this.redirectFormSubmitInit();
-        }
-
-        paymentSuccessPageConsume() {
-            return this.redirectFormSubmit();
-        }
-
-
-        // --
-
 
         async asyncFormSubmit() {
+            await this.redirectFormSubmitInit();
+            return await this.redirectFormSubmit();
+        }
 
-            // if (this.isCampaignPage) {
-            //     return;
-            // }
+        // --
 
-            const formUrlParams = new URLSearchParams
-            formUrlParams.append('campaign_code', this.camp_code);
+        async paymentPageInit() {
+            return await this.redirectFormSubmitInit();
+        }
 
-            try {
-                const res = await this.makeApiCall(formUrlParams, 'Async Form campaign success !!')
-                console.log(res);
-
-                return res;
-            } catch (err) {
-                console.log(err);
-
-                return err;
-            }
+        async paymentSuccessPageConsume() {
+            return await this.redirectFormSubmit();
         }
 
         // --
 
 
-        download(id) {
-
-            // if (!this.isCampaignPage) {
-            //     return;
-            // }
-
+        async download(id) {
             const element = document.getElementById(id);
 
-            element.addEventListener('click', async() => {
-                const formUrlParams = new URLSearchParams;
-                formUrlParams.append('campaign_code', this.camp_code);
-
-                try {
-                    const res = await this.makeApiCall(formUrlParams, 'Download campaign success !!');
-                    console.log(res);
-
-                    return res;
-                } catch (err) {
-                    console.log(err);
-
-                    return err;
-                }
+            element.addEventListener('click', async () => {
+                await this.redirectFormSubmitInit();
+                return await this.redirectFormSubmit();
             });
-
         }
 
-        clickAction(id) {
-            return this.download(id);
+        async clickAction(id) {
+            return await this.download(id);
         }
-
 
         // --
-
 
         /**
          * Makes an api call to adbirt backend
@@ -153,9 +172,7 @@
          * @returns {Promise}
          */
         async makeApiCall(reqBody, msg) {
-
             try {
-
                 const res = await fetch(this.url, {
                     method: "POST",
                     headers: {
@@ -165,24 +182,27 @@
                 });
 
                 const json = await res.json();
-
                 console.log(json);
 
                 return json;
             } catch (error) {
                 return error;
             }
-
         }
 
+        // --
+
+        noCampaignSpecified() {
+            console.log('No campaign specified');
+        }
     }
 
     /**
-     * @type {Adbirt} {@link Adbirt}
+     * Kickstart the script
      */
-    let AB = null;
     try {
-        AB = new Adbirt;
+        console.log('Kickstart');
+        const AB = new AdbirtAdvertiser;
 
         if (window.AB) {
             window._Adbirt = AB;
@@ -193,7 +213,6 @@
         window.adbirtNoConflict = () => {
             return AB;
         }
-
     } catch (err) {
         console.log(err);
     }
